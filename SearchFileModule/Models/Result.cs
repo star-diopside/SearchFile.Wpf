0a5d66af26f468f1780d469fa1d8d70974d5736d
se-yoshi <se.yoshi@gmail.com>
2016-06-12 00:27:22 +0900

    ターゲットとする.NET Frameworkバージョンの変更 ファイルアイコン表示機能の追加

--- SearchFileModule/Models/Result.cs
+++ SearchFileModule/Models/Result.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using Microsoft.WindowsAPICodePack.Shell;
+using NLog;
+using System.IO;
+using System.Windows.Media;
 
 namespace SearchFile.Models
 {
@@ -7,23 +10,44 @@ namespace SearchFile.Models
     /// </summary>
     public class Result
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         /// <summary>
-        /// ファイル名を取得または設定する。
+        /// ファイル名を取得する。
         /// </summary>
         public string FileName => Path.GetFileName(this.FilePath);
 
         /// <summary>
-        /// 拡張子を取得または設定する。
+        /// 拡張子を取得する。
         /// </summary>
         public string Extension => Path.GetExtension(this.FilePath);
 
         /// <summary>
-        /// ディレクトリ名を取得または設定する。
+        /// ディレクトリ名を取得する。
         /// </summary>
         public string DirectoryName => Path.GetDirectoryName(this.FilePath);
 
         /// <summary>
-        /// ファイルパスを取得または設定する。
+        /// ファイルに関連付けられたアイコンを取得する。
+        /// </summary>
+        public ImageSource IconSource
+        {
+            get
+            {
+                try
+                {
+                    return ShellFile.FromFilePath(this.FilePath).Thumbnail.SmallBitmapSource;
+                }
+                catch (ShellException ex)
+                {
+                    logger.Warn(ex, ex.Message);
+                    return null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// ファイルパスを取得する。
         /// </summary>
         public string FilePath { get; }
 
